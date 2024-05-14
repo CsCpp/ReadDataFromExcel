4fdce662e8d2441420e3fc5f72ef0e7c84a5afe8
CppKotlin <cscppkotlin@gmail.com>
2024-05-14 23:36:14 +0300

    хз не работает

--- ReadDataFromExcel/Form1.Designer.cs
+++ ReadDataFromExcel/Form1.Designer.cs
@@ -32,41 +32,73 @@
             contextMenuStrip1 = new ContextMenuStrip(components);
             файлToolStripMenuItem = new ToolStripMenuItem();
             открытьToolStripMenuItem = new ToolStripMenuItem();
+            открытьToolStripMenuItem1 = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            closeToolStripMenuItem = new ToolStripMenuItem();
             toolStrip1 = new ToolStrip();
             toolStripLabel1 = new ToolStripLabel();
             toolStripComboBox1 = new ToolStripComboBox();
             dataGridView1 = new DataGridView();
             openFileDialog1 = new OpenFileDialog();
+            menuStrip1 = new MenuStrip();
+            файлToolStripMenuItem1 = new ToolStripMenuItem();
+            открытьToolStripMenuItem2 = new ToolStripMenuItem();
             contextMenuStrip1.SuspendLayout();
             toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // contextMenuStrip1
             // 
             contextMenuStrip1.ImageScalingSize = new Size(20, 20);
-            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem });
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem, openToolStripMenuItem, closeToolStripMenuItem });
             contextMenuStrip1.Name = "contextMenuStrip1";
-            contextMenuStrip1.Size = new Size(115, 28);
+            contextMenuStrip1.ShowCheckMargin = true;
+            contextMenuStrip1.Size = new Size(137, 82);
+            contextMenuStrip1.Text = "Menu";
+            contextMenuStrip1.Opening += contextMenuStrip1_Opening;
             // 
             // файлToolStripMenuItem
             // 
-            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { открытьToolStripMenuItem });
+            файлToolStripMenuItem.Checked = true;
+            файлToolStripMenuItem.CheckState = CheckState.Checked;
+            файлToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { открытьToolStripMenuItem, открытьToolStripMenuItem1 });
             файлToolStripMenuItem.Name = "файлToolStripMenuItem";
-            файлToolStripMenuItem.Size = new Size(114, 24);
+            файлToolStripMenuItem.Size = new Size(136, 26);
             файлToolStripMenuItem.Text = "Файл";
+            файлToolStripMenuItem.Click += файлToolStripMenuItem_Click;
             // 
             // открытьToolStripMenuItem
             // 
             открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
-            открытьToolStripMenuItem.Size = new Size(224, 26);
+            открытьToolStripMenuItem.Size = new Size(150, 26);
             открытьToolStripMenuItem.Text = "Открыть";
+            открытьToolStripMenuItem.Click += открытьToolStripMenuItem_Click;
+            // 
+            // открытьToolStripMenuItem1
+            // 
+            открытьToolStripMenuItem1.Name = "открытьToolStripMenuItem1";
+            открытьToolStripMenuItem1.Size = new Size(150, 26);
+            открытьToolStripMenuItem1.Text = "Открыть";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(136, 26);
+            openToolStripMenuItem.Text = "Open";
+            // 
+            // closeToolStripMenuItem
+            // 
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.Size = new Size(136, 26);
+            closeToolStripMenuItem.Text = "Close";
             // 
             // toolStrip1
             // 
             toolStrip1.ImageScalingSize = new Size(20, 20);
             toolStrip1.Items.AddRange(new ToolStripItem[] { toolStripLabel1, toolStripComboBox1 });
-            toolStrip1.Location = new Point(0, 0);
+            toolStrip1.Location = new Point(0, 28);
             toolStrip1.Name = "toolStrip1";
             toolStrip1.Size = new Size(1322, 28);
             toolStrip1.TabIndex = 1;
@@ -83,21 +115,47 @@
             // 
             toolStripComboBox1.Name = "toolStripComboBox1";
             toolStripComboBox1.Size = new Size(121, 28);
+            toolStripComboBox1.SelectedIndexChanged += toolStripComboBox1_SelectedIndexChanged;
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Dock = DockStyle.Fill;
-            dataGridView1.Location = new Point(0, 28);
+            dataGridView1.Location = new Point(0, 56);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(1322, 422);
+            dataGridView1.Size = new Size(1322, 394);
             dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // openFileDialog1
             // 
             openFileDialog1.FileName = "openFileDialog1";
             openFileDialog1.Filter = "Excel|*.xlsx";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { файлToolStripMenuItem1 });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1322, 28);
+            menuStrip1.TabIndex = 3;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem1
+            // 
+            файлToolStripMenuItem1.DropDownItems.AddRange(new ToolStripItem[] { открытьToolStripMenuItem2 });
+            файлToolStripMenuItem1.Name = "файлToolStripMenuItem1";
+            файлToolStripMenuItem1.Size = new Size(59, 24);
+            файлToolStripMenuItem1.Text = "Файл";
+            // 
+            // открытьToolStripMenuItem2
+            // 
+            открытьToolStripMenuItem2.Name = "открытьToolStripMenuItem2";
+            открытьToolStripMenuItem2.Size = new Size(224, 26);
+            открытьToolStripMenuItem2.Text = "Открыть";
+            открытьToolStripMenuItem2.Click += открытьToolStripMenuItem2_Click;
             // 
             // Form1
             // 
@@ -106,6 +164,8 @@
             ClientSize = new Size(1322, 450);
             Controls.Add(dataGridView1);
             Controls.Add(toolStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
@@ -113,13 +173,13 @@
             toolStrip1.ResumeLayout(false);
             toolStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-
-        private ContextMenuStrip contextMenuStrip1;
         private ToolStripMenuItem файлToolStripMenuItem;
         private ToolStripMenuItem открытьToolStripMenuItem;
         private ToolStrip toolStrip1;
@@ -127,5 +187,12 @@
         private ToolStripComboBox toolStripComboBox1;
         private DataGridView dataGridView1;
         private OpenFileDialog openFileDialog1;
+        private ToolStripMenuItem открытьToolStripMenuItem1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem файлToolStripMenuItem1;
+        private ToolStripMenuItem открытьToolStripMenuItem2;
     }
 }
--- ReadDataFromExcel/Form1.cs
+++ ReadDataFromExcel/Form1.cs
@@ -1,4 +1,5 @@
 using ExcelDataReader;
+using System.Data;
 
 
 namespace ReadDataFromExcel
@@ -6,8 +7,8 @@ namespace ReadDataFromExcel
     public partial class Form1 : Form
 
     {
-        private string fileName=string.Empty;
-
+        private string fileName = string.Empty;
+        private DataTableCollection tableCollection = null;
 
         public Form1()
         {
@@ -22,6 +23,74 @@ namespace ReadDataFromExcel
         private void toolStripLabel1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void ÓÚÍ˚Ú¸ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+        }
+        private void OpenExcelFile(string path)
+        {
+            FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read);
+            IExcelDataReader reader = ExcelReaderFactory.CreateReader(stream);
+            DataSet db = reader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (x) => new ExcelDataTableConfiguration()
+                {
+                    UseHeaderRow = true,
+                }
+            });
+            tableCollection = db.Tables;
+            toolStripComboBox1.Items.Clear();
+            foreach (DataTable tabe in tableCollection)
+            {
+                toolStripComboBox1.Items.Add(tabe.TableName);
+            }
+            toolStripComboBox1.SelectedIndex = 0;
+        }
+
+        private void toolStripComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable table = tableCollection[Convert.ToString(toolStripComboBox1.SelectedItem)];
+            dataGridView1.DataSource = table;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Ù‡ÈÎToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ÓÚÍ˚Ú¸ToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                DialogResult res = openFileDialog1.ShowDialog();
+                if (res == DialogResult.OK)
+                {
+                    fileName = openFileDialog1.FileName;
+                    Text = fileName;
+                    OpenExcelFile(fileName);
+                }
+                else
+                {
+                    throw new Exception("‘‡ÈÎ ÌÂ ‚˚·‡Ì!!!");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
